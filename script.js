32f6df085244d098a2482e177ac88e2726224d56
Muhammad Aqeel Jabbar <aqeeljabbr19@gmail.com>
2024-04-05 07:37:01 +0700

    V.5 Buat Conditional

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ let y = 5;
 function Tambah() {
     x+y
 }
+
+if (x < y) {
+    console.log("Gutten Tag!") ;
+  } else if (x = 20) {
+    console.log("Alle Gute!");
+  } else {
+    console.log("Guten Morgen!");
+  }
